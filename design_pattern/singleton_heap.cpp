9bf144be2e3f024b19ab69ca038210fa89c8f6ca
SBWOO <dntkdqja12@gmail.com>
2022-04-14 10:03:49 +0900

    added singleton_heap.cpp for practice

--- design_pattern/singleton_heap.cpp
+++ design_pattern/singleton_heap.cpp
@@ -6,9 +6,12 @@ class SingletonClass
 {
   ~SingletonClass() = default;
 public:
-  static SingletonClass& getInstance(int input){
-    static SingletonClass s;
-    s.class_id_ = input;
+  static SingletonClass* getInstance(int input){
+    // NULL일 경우 전역변수 해제
+    if (s == NULL) {
+      s = new SingletonClass;
+    }
+    s -> class_id_ = input;
     return s;
   }
   void print()
@@ -20,17 +23,19 @@ private:
   SingletonClass() {}
   SingletonClass(const SingletonClass& ref) = delete;
   SingletonClass& operator=(SingletonClass& in) = delete;
-
+  static SingletonClass * s;
   int class_id_;
 };
+// NULL 포인터로 선언된 전역변수가 존재
+SingletonClass* SingletonClass::s = NULL;
 
 int main(int argc, char * argv[])
 {
-  SingletonClass& s = SingletonClass::getInstance(1);
-  s.print();
+  SingletonClass* s = SingletonClass::getInstance(1);
+  s->print();
 
-  SingletonClass& s1 = SingletonClass::getInstance(2);
-  s1.print();
-  s.print();
+  SingletonClass* s1 = SingletonClass::getInstance(2);
+  s1->print();
+  s->print();
   return 0;
 }
