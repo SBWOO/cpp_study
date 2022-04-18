587784d53d32e8d75919da489b01375c8ba8b398
SBWOO <dntkdqja12@gmail.com>
2022-04-19 08:59:41 +0900

    proxy에 주석추가

--- design_pattern/proxy.cpp
+++ design_pattern/proxy.cpp
@@ -5,25 +5,27 @@ public:
   virtual void request() = 0;
 };
 
-class RealSubject : public Subject{
+// 여기서부터 주석하고 연습
+class RealSubject : public Subject {
 public:
   void request() {std::cout << "RealSubject Request" << std::endl;}
 };
 
-class Proxy : public Subject{
+class Porxy : public Subject {
 public:
   Proxy() : _realsubject(nullptr) {}
   ~Proxy() {if(_realsubject) delete _realsubject; }
 
   void request() {
     if(!_realsubject)
-      _realsubject = new RealSubject;
+     _realsubject = new RealSubject;
 
-    _realsubject->request();
+     _realsubject->request();
   }
 private:
   RealSubject* _realsubject;
 };
+// 여기까지 주석
 
 int main() {
   Subject* Proxyptr = new Proxy();
